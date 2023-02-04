d07b27d6dbb8ed3967155abbe70e520b3688fcd2
digimark2023 <digimark2023@gmail.com>
2023-02-04 20:08:19 +0530

    chnage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //fetch("https://digimark2023.github.io/chatgpt/", {
+    // fetch("https://digimark2023.github.io/chatgpt/", {
     fetch("http://localhost:3001/", {
       method: "POST",
       headers: {
